956e1821ddf0393df8d863b07858abd1aa626cce
Diamond White <diamondalonzo@gmail.com>
2026-02-13 20:15:20 +0000

    player controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,28 +8,36 @@ namespace DefaultNamespace
     {
         public float speed;
         public float jumpForce;
+        public float lookSensitivity;
 
         private InputAction _move;
         private InputAction _jump;
+        private InputAction _look;
         private Rigidbody _rb;
 
         public void Awake()
         {
             _move = InputSystem.actions.FindAction("Move");
             _jump = InputSystem.actions.FindAction("Jump");
+            _look = InputSystem.actions.FindAction("Look");
             _rb = GetComponent<Rigidbody>();
         }
 
-        public void Update()
+        public void FixedUpdate()
         {
-            var input = _move.ReadValue<Vector2>();
-            var horizontalMovement = input * (speed * Time.deltaTime);
+            var moveValue = _move.ReadValue<Vector2>();
+            var horizontalMovement = moveValue * (speed * Time.fixedDeltaTime);
             var newPosition = _rb.position + new Vector3(horizontalMovement.x, 0, horizontalMovement.y);
             _rb.MovePosition(newPosition);
 
+            var lookValue = _look.ReadValue<Vector2>();
+            var newRotation =
+                _rb.rotation * Quaternion.Euler(0, lookValue.y * lookSensitivity * Time.fixedDeltaTime, 0);
+            _rb.MoveRotation(newRotation);
+
             if (_jump.triggered)
             {
-                _rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Force);
+                _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
         }
     }
